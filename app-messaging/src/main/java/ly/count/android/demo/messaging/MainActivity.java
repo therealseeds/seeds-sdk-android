0007c504b4abe4bd6b22813e181271dce6551cae
Sungwon Choe <sungwon.peter.choe@gmail.com>
2015-05-05 20:16:14 -0400

    implemented button to show in-app message

--- app-messaging/src/main/java/ly/count/android/demo/messaging/MainActivity.java
+++ app-messaging/src/main/java/ly/count/android/demo/messaging/MainActivity.java
@@ -8,23 +8,40 @@ import android.content.IntentFilter;
 import android.os.Handler;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import ly.count.android.sdk.Countly;
+import ly.count.android.sdk.inappmessaging.Ad;
+import ly.count.android.sdk.inappmessaging.AdListener;
+import ly.count.android.sdk.inappmessaging.AdManager;
 import ly.count.android.sdk.messaging.CountlyMessaging;
 import ly.count.android.sdk.messaging.Message;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements AdListener {
 
     private static String YOUR_SERVER = "http://ec2-52-7-34-112.compute-1.amazonaws.com/";
     private static String YOUR_APP_KEY = "b1eed6c8bf769ffded7332893b62b8e6f4d73a32";
 
+    private static final String MOB_FOX_PUB_ID = "86e0aa6e7fbd2cdb02ec15e338d6b722";
+    private static final String MOB_FOX_AD_URL = "http://my.mobfox.com/request.php";
+
     private BroadcastReceiver messageReceiver;
+
+    private Button iamButton;
+    private AdManager manager;
 
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        iamButton = (Button) findViewById(R.id.iamButton);
+        manager = new AdManager(this, MOB_FOX_AD_URL, MOB_FOX_PUB_ID, false);
+        manager.setListener(this);
 
         /** You should use cloud.count.ly instead of YOUR_SERVER for the line below if you are using Countly Cloud service */
         Countly.sharedInstance()
@@ -86,5 +103,56 @@ public class MainActivity extends Activity {
     protected void onPause() {
         super.onPause();
         unregisterReceiver(messageReceiver);
+    }
+
+    public void iamButtonClicked(View view) {
+        showInAppMessage();
+    }
+
+
+    public void showInAppMessage() {
+        try {
+            runOnUiThread(new Runnable() {
+                public void run() {
+                    if (manager.isAdLoaded()) {
+                        manager.showAd();
+
+
+                    } else {
+                        manager.requestAd();
+//                        Toast.makeText(AndroidLauncher.this, "Ad loading...", Toast.LENGTH_LONG)
+//                                .show();
+                    }
+                }
+            });
+        } catch (Exception e) {
+            System.out.println("Exception: " + e);
+        }
+    }
+
+
+    @Override
+    public void adClicked() {
+
+    }
+
+    @Override
+    public void adClosed(Ad ad, boolean completed) {
+
+    }
+
+    @Override
+    public void adLoadSucceeded(Ad ad) {
+
+    }
+
+    @Override
+    public void adShown(Ad ad, boolean succeeded) {
+
+    }
+
+    @Override
+    public void noAdFound() {
+
     }
 }
