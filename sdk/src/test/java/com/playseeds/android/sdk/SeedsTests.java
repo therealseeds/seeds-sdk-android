113e9110373c1e7350ffe4dd651eed02d71049e6
Alexey Pelykh <alexey.pelykh@gmail.com>
2015-10-01 00:07:22 +0300

    Tests

--- sdk/src/test/java/com/playseeds/android/sdk/SeedsTests.java
+++ sdk/src/test/java/com/playseeds/android/sdk/SeedsTests.java
@@ -3,6 +3,7 @@ package com.playseeds.android.sdk;
 import com.playseeds.android.sdk.inappmessaging.InAppMessage;
 import com.playseeds.android.sdk.inappmessaging.InAppMessageListener;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -46,142 +47,72 @@ public class SeedsTests {
                         SERVER, UNLIMITED_ADS_APP_KEY, null, DeviceId.Type.ADVERTISING_ID);
     }
 
+    private class InAppMessageLoadListener implements InAppMessageListener {
+        private Boolean wasLoaded = null;
+
+        public boolean hasResult() {
+            return wasLoaded != null;
+        }
+
+        public boolean getWasLoaded() throws Exception {
+            if (wasLoaded == null)
+                throw new Exception();
+            return wasLoaded;
+        }
+
+        @Override
+        public void inAppMessageClicked() {}
+
+        @Override
+        public void inAppMessageClosed(InAppMessage inAppMessage, boolean completed) {}
+
+        @Override
+        public void inAppMessageLoadSucceeded(InAppMessage inAppMessage) {
+            synchronized (this) {
+                wasLoaded = true;
+                notifyAll();
+            }
+        }
+
+        @Override
+        public void inAppMessageShown(InAppMessage inAppMessage, boolean succeeded) {}
+
+        @Override
+        public void noInAppMessageFound() {
+            synchronized (this) {
+                wasLoaded = false;
+                notifyAll();
+            }
+        }
+    }
+
     @Test
-    public void testSeedInAppMessageShown() {
-        InAppMessageListener listener = new InAppMessageListener() {
-            @Override
-            public void inAppMessageClicked() {
-
-            }
-
-            @Override
-            public void inAppMessageClosed(InAppMessage inAppMessage, boolean completed) {
-
-            }
-
-            @Override
-            public void inAppMessageLoadSucceeded(InAppMessage inAppMessage) {
-
-            }
-
-            @Override
-            public void inAppMessageShown(InAppMessage inAppMessage, boolean succeeded) {
-
-            }
-
-            @Override
-            public void noInAppMessageFound() {
-
-            }
-        };
+    public void testSeedInAppMessageLoadSucceeded() throws InterruptedException, Exception {
+        InAppMessageLoadListener listener = new InAppMessageLoadListener();
 
         Seeds.sharedInstance()
                 .init(ShadowApplication.getInstance().getApplicationContext(), listener,
                         SERVER, UNLIMITED_ADS_APP_KEY);
         Seeds.sharedInstance().requestInAppMessage();
-
-//        WelcomeActivity activity = Robolectric.setupActivity(WelcomeActivity.class);
-//        activity.findViewById(R.id.login).performClick();
-//
-//        Intent expectedIntent = new Intent(activity, WelcomeActivity.class);
-//        assertThat(shadowOf(activity).getNextStartedActivity()).isEqualTo(expectedIntent);
+        synchronized (listener) {
+            if (!listener.hasResult())
+                listener.wait(10000);
+        }
+        Assert.assertTrue(listener.getWasLoaded());
     }
 
-//
-//    - (void)testSeedInAppMessageShown {
-//
-//        [Seeds.sharedInstance start:YOUR_APP_KEY withHost:YOUR_SERVER];
-//
-//        NSDate *fiveSeconds = [NSDate dateWithTimeIntervalSinceNow:5.0];
-//
-//        if ([[Seeds sharedInstance] isInAppMessageLoaded]) {
-//            [[Seeds sharedInstance] showInAppMessageIn:_testVC];
-//        } else {
-//            [[Seeds sharedInstance] requestInAppMessage];
-//        }
-//
-//        [[NSRunLoop currentRunLoop] runUntilDate:fiveSeconds];
-//
-//        XCTAssertTrue(_seedsInAppMessageShown, @"in app message not shown");
-//        XCTAssertTrue(_seedsInAppMessageLoaded, @"not loaded");
-//        XCTAssertFalse(_seedsNotFound, @"not found");
-//
-//    }
-//
-//    - (void)testSeedInAppMessageShownNeverAds {
-//
-//        [[Seeds sharedInstance] start:YOUR_APP_KEY_NEVER withHost:YOUR_SERVER];
-//
-//        NSDate *fiveSeconds = [NSDate dateWithTimeIntervalSinceNow:5.0];
-//
-//        if ([[Seeds sharedInstance] isInAppMessageLoaded]) {
-//            [[Seeds sharedInstance] showInAppMessageIn:_testVC];
-//        } else {
-//            [[Seeds sharedInstance] requestInAppMessage];
-//        }
-//
-//        [[NSRunLoop currentRunLoop] runUntilDate:fiveSeconds];
-//
-//        XCTAssertFalse(_seedsInAppMessageLoadedNever, @"Message loaded");
-//
-//    }
-//
-//    - (void)testSeedInAppMessageShownAlwaysAds {
-//
-//        [[Seeds sharedInstance] start:YOUR_APP_KEY_ALWAYS withHost:YOUR_SERVER];
-//
-//        NSDate *fiveSeconds = [NSDate dateWithTimeIntervalSinceNow:5.0];
-//
-//        if ([[Seeds sharedInstance] isInAppMessageLoaded]) {
-//            [[Seeds sharedInstance] showInAppMessageIn:_testVC];
-//        } else {
-//            [[Seeds sharedInstance] requestInAppMessage];
-//        }
-//
-//        [[NSRunLoop currentRunLoop] runUntilDate:fiveSeconds];
-//
-//        XCTAssertTrue(_seedsInAppMessageLoadedAlways, @"Message not loaded");
-//
-//    }
-//
-//    #pragma mark - SeedsInAppMessageDelegate
-//
-//    - (void)seedsInAppMessageShown:(SeedsInAppMessage*)inAppMessage withSuccess:(BOOL)success {
-//
-//        if ([[[Seeds sharedInstance] getAppKey] isEqualToString:YOUR_APP_KEY]) {
-//            _seedsInAppMessageShown = success;
-//        } else if ([[[Seeds sharedInstance] getAppKey] isEqualToString:YOUR_APP_KEY_NEVER]) {
-//            _seedsInAppMessageShownNever = success;
-//        } else if ([[[Seeds sharedInstance] getAppKey] isEqualToString:YOUR_APP_KEY_ALWAYS]) {
-//            _seedsInAppMessageShownAlways = success;
-//        }
-//    }
-//
-//    - (void)seedsInAppMessageLoadSucceeded:(SeedsInAppMessage*)inAppMessage {
-//
-//        if ([[[Seeds sharedInstance] getAppKey] isEqualToString:YOUR_APP_KEY]) {
-//            _seedsInAppMessageLoaded = YES;
-//        } else if ([[[Seeds sharedInstance] getAppKey] isEqualToString:YOUR_APP_KEY_NEVER]) {
-//            _seedsInAppMessageLoadedNever = YES;
-//        } else if ([[[Seeds sharedInstance] getAppKey] isEqualToString:YOUR_APP_KEY_ALWAYS]) {
-//            _seedsInAppMessageLoadedAlways = YES;
-//        }
-//
-//        [[Seeds sharedInstance] showInAppMessageIn:_testVC];
-//
-//    }
-//
-//
-//    - (void)seedsNoInAppMessageFound {
-//
-//        if ([[[Seeds sharedInstance] getAppKey] isEqualToString:YOUR_APP_KEY]) {
-//            _seedsNotFound = YES;
-//        } else if ([[[Seeds sharedInstance] getAppKey] isEqualToString:YOUR_APP_KEY_NEVER]) {
-//            _seedsNotFoundNever = YES;
-//        } else if ([[[Seeds sharedInstance] getAppKey] isEqualToString:YOUR_APP_KEY_ALWAYS]) {
-//            _seedsNotFoundAlways = YES;
-//        }
-//    }
-//
+    @Test
+    public void testSeedInAppMessageLoadFailed() throws InterruptedException, Exception {
+        InAppMessageLoadListener listener = new InAppMessageLoadListener();
 
+        Seeds.sharedInstance()
+                .init(ShadowApplication.getInstance().getApplicationContext(), listener,
+                        SERVER, NO_ADS_APP_KEY);
+        Seeds.sharedInstance().requestInAppMessage();
+        synchronized (listener) {
+            if (!listener.hasResult())
+                listener.wait(10000);
+        }
+        Assert.assertFalse(listener.getWasLoaded());
+    }
 }
