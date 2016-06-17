ac816b81c53c55a649633f498b6a12579523de5e
andela-oofoamalu <jessicaprincess61@gmail.com>
2016-06-17 16:35:07 +0100

    Fix failing test

--- sdk/src/androidTest/java/com/playseeds/android/sdk/inappmessaging/InAppMessageViewTest.java
+++ sdk/src/androidTest/java/com/playseeds/android/sdk/inappmessaging/InAppMessageViewTest.java
@@ -1,17 +1,12 @@
 package com.playseeds.android.sdk.inappmessaging;
 
-import android.content.Context;
 import android.test.AndroidTestCase;
 
 public class InAppMessageViewTest extends AndroidTestCase {
-    InAppMessageView inAppMessageView;
-    InAppMessageResponse response;
-    InAppMessageView.BannerAdViewListener viewListener;
 
-    public void setUp() throws Exception {
-        Context context = getContext();
-        response = new InAppMessageResponse();
-        viewListener = new InAppMessageView.BannerAdViewListener() {
+    public void testConstructor() throws Exception {
+        InAppMessageResponse response = new InAppMessageResponse();
+        InAppMessageView.BannerAdViewListener viewListener = new InAppMessageView.BannerAdViewListener() {
             @Override
             public void onLoad() {
             }
@@ -25,10 +20,7 @@ public class InAppMessageViewTest extends AndroidTestCase {
             }
         };
 
-        inAppMessageView = new InAppMessageView(context, response, 0, 0, false, viewListener);
-    }
-
-    public void testConstructor() throws Exception {
+        InAppMessageView inAppMessageView = new InAppMessageView(getContext(), response, 0, 0, false, viewListener);
         assertSame(viewListener, inAppMessageView.getAdListener());
         assertSame(response, inAppMessageView.getResponse());
     }
